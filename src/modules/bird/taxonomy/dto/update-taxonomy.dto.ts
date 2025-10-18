b5fe6b3bc4cd4fb03b43c0a20a3fe3819982469e
Arash1Kazemi <arashkazemi7l5o@gmail.com>
2025-10-18 19:01:51 +0330

    add missing DTOs

--- src/modules/bird/taxonomy/dto/update-taxonomy.dto.ts
+++ src/modules/bird/taxonomy/dto/update-taxonomy.dto.ts
@@ -1,1 +1,4 @@
-export class UpdateTaxonomyDto {}
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateTaxonomyDto } from './create-taxonomy.dto';
+
+export class UpdateTaxonomyDto extends PartialType(CreateTaxonomyDto) {}
