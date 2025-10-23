9bcd21f4e1e7d3203598c92ec2bcf3a350e75a95
Arash1Kazemi <arashkazemi7l5o@gmail.com>
2025-10-23 21:18:24 +0330

    start Data collector Controler

--- src/modules/data-collector/data-collector.module.ts
+++ src/modules/data-collector/data-collector.module.ts
@@ -3,8 +3,8 @@ import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
 import { DataCollectorService } from './data-collector.service';
 import { MediaModule } from '../media/media.module';
-import { DataProcessorService } from '../data-processor/data-processor/data-processor.service';
 import { BirdsModule } from '../bird/birds/birds.module';
+import { DataProcessorService } from '../data-processor/data-processor.service';
 
 @Module({
     imports: [HttpModule, ConfigModule, BirdsModule, MediaModule],
