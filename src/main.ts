4f6d1d681759a801e21c3c96d40cd3c289b26dc5
Arash1Kazemi <arashkazemi7l5o@gmail.com>
2025-11-14 16:11:02 +0330

    Change migratin and main.ts for better log

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,39 @@
-import { NestFactory, Reflector } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { join } from 'path';
-import * as express from 'express';
-import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
-    app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
+    // Enable CORS
+    app.enableCors({
+        origin: '*', // Change this in production
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        credentials: true,
+    });
 
-    app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector))); // Or use plainToInstance IN controler
+    // Global validation pipe
     app.useGlobalPipes(
         new ValidationPipe({
             whitelist: true,
-            forbidNonWhitelisted: true,
             transform: true,
         }),
     );
 
+    // Swagger setup
     const config = new DocumentBuilder()
-        .setTitle('Bird API')
-        .setDescription('Comprehensive bird information API')
+        .setTitle('Bird Identifier API')
+        .setDescription('Bird Identifier Backend API')
         .setVersion('1.0')
-        .addTag('birds')
         .build();
-
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, document);
 
-    await app.listen(process.env.PORT ?? 3000);
+    const port = process.env.PORT || 3000;
+    await app.listen(port, '0.0.0.0'); // Important: listen on 0.0.0.0
+
+    console.log(`🚀 Application is running on: http://localhost:${port}`);
+    console.log(`📚 Swagger documentation: http://localhost:${port}/api`);
 }
-void bootstrap();
+bootstrap();
