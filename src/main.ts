aa1c96196aa72d78746732497cd7b7c79ae50306
Arash1Kazemi <arashkazemi7l5o@gmail.com>
2025-12-15 19:44:38 +0330

    PATCH: change to use local postgres

--- src/main.ts
+++ src/main.ts
@@ -47,7 +47,7 @@ async function bootstrap() {
     const port = process.env.PORT || 3000;
     await app.listen(port, '0.0.0.0');
     console.log(` Application is running on: http://0.0.0.0:${port}`);
-    console.log(`check new server `);
+    console.log(` Application is running on: http://0.0.0.0:${port}`);
 
 }
 bootstrap();
